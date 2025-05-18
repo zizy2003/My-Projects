8501f0b845437d2860cb382a9dc391e192e5ff3b
zizy2003 <leshairnazarov27@gmail.com>
2025-05-18 19:11:00 -0400

    .

--- NetflixMoviesAndTVShows/sql_queries.sql
+++ NetflixMoviesAndTVShows/sql_queries.sql
@@ -1,11 +1,11 @@
-CREATE DATABASE netflixmoviesdata;
+-- CREATE DATABASE netflixmoviesdata;
 
 CREATE TABLE IF NOT EXISTS netflix_titles (
-show_id VARCHAR(10) PRIMARY KEY,
-type VARCHAR(10) NOT NULL,
+show_id VARCHAR(20) PRIMARY KEY,
+type2 VARCHAR(10) NOT NULL,
 title TEXT NOT NULL,
 director TEXT ,
-cast TEXT ,
+cast_members TEXT,
 country TEXT ,
 date_added DATE ,
 release_year INTEGER NOT NULL,
@@ -16,3 +16,23 @@ description TEXT NOT NULL
 
 
 );
+-- Quick check for a table
+SELECT * FROM netflix_titles;
+
+-- Changing data type of date_added column
+ALTER TABLE netflix_titles
+ALTER COLUMN date_added TYPE TEXT;
+
+--Changing name of type column
+ALTER TABLE netflix_titles
+RENAME COLUMN "type" TO type2;
+
+-- Changing type of rating column
+ALTER TABLE netflix_titles
+ALTER COLUMN rating TYPE VARCHAR(10);
+
+-- Changing condition to import data (allow null values)
+ALTER TABLE public.netflix_titles ALTER COLUMN duration DROP NOT NULL;
+
+
+
